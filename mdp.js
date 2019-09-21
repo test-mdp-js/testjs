31e1658b8da90f562bdb72b55c952e0412830b46
test-mdp-js <55623029+test-mdp-js@users.noreply.github.com>
2019-09-21 13:43:26 +0200

    Update mdp.js

--- mdp.js
+++ mdp.js
@@ -1,6 +1,6 @@
 var btn = document.getElementById('validate');
 
-var sec_resultCheckMDP = new Boolean(0);
+var sec_resultCheckMDP = new Boolean(1);
 
 var ID_BLOC_PASSWORD_SPECIAL_CHARCTER = "#blocPasswordSpecialCharcter";
 
@@ -9,10 +9,6 @@ var ID_BLOC_BLOC_PASSWORD_MORE_OR_LESS_8 = "#blocPasswordMoreOrLess8";
 var ID_BLOC_PASSWORD_MATCHED = "#blocPasswordMatched";
 
 var rules = [
-    {
-        idBloc: ID_BLOC_PASSWORD_UPPER_LOW_CAS,
-        rule: 'new RegExp("(?:[A-Z].*){1}").test($(".newPassword").val()) && new RegExp("(?:[a-z].*){1}").test($(".newPassword").val())'
-    },
     {
         idBloc: ID_BLOC_PASSWORD_SPECIAL_CHARCTER,
         rule: '(/^[0-9a-zA-Z]*$/g.test($(".newPassword").val()) && $(".newPassword").val().length > 0)'
@@ -29,7 +25,6 @@ var rules = [
 
 
 $(".newPassword, .newPasswordConfirmation").keyup(function () {
-    sec_resultCheckMDP = 1;
     rules.forEach(function (item) {
         if (eval(item.rule)) {
             blocValide(item.idBloc);
@@ -44,7 +39,6 @@ $(".newPassword, .newPasswordConfirmation").keyup(function () {
 function blocInvalide(idBloc) {
     $(idBloc).find('i').removeClass().addClass("sr-invalide-password");
     $(idBloc).find('span').removeClass().addClass("sr-item-text-grey");
-    sec_resultCheckMDP = 0;
 }
 
 function blocValide(idBloc) {
